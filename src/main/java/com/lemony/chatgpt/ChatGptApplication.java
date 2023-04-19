6c96bb970334076509d100b9b7fdaa76e2588c65
Ulemony <2669315895@qq.com>
2023-04-19 22:02:27 +0800

    新增SpringContextHolder

--- src/main/java/com/lemony/chatgpt/ChatGptApplication.java
+++ src/main/java/com/lemony/chatgpt/ChatGptApplication.java
@@ -1,13 +1,19 @@
 package com.lemony.chatgpt;
 
 
+import com.lemony.chatgpt.util.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 
 public class ChatGptApplication {
+    @Bean
+    public SpringContextHolder springContextHolder() {
+        return new SpringContextHolder();
+    }
     public static void main(String[] args) {
        SpringApplication.run(ChatGptApplication.class,args);
     }
